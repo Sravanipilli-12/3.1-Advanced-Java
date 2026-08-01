c6579032ae486b4d0107aad6eacea6370be59488
Sravanipilli-12 <25pa5a1227@vishnu.edu.in>
2026-08-01 14:49:51 +0530

    Add files via upload

---
+++ SResultSet.java
@@ -0,0 +1,93 @@
+import java.sql.*;
+
+public class SResultSet {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "testuser";
+        String password = "testpass";
+
+        try {
+
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connect to MySQL database
+            Connection con = DriverManager.getConnection(url, user, password);
+
+            // Create Scrollable ResultSet
+            Statement st = con.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+
+            // Execute query
+            ResultSet rs = st.executeQuery("SELECT * FROM Student");
+
+            System.out.println("Records in Forward Direction:");
+            System.out.println("RollNo\tName\tAddress");
+            System.out.println("-----------------------------");
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getInt("RollNo") + "\t" +
+                        rs.getString("Name") + "\t" +
+                        rs.getString("Address"));
+            }
+
+            System.out.println("\nRecords in Backward Direction:");
+            System.out.println("RollNo\tName\tAddress");
+            System.out.println("-----------------------------");
+
+            while (rs.previous()) {
+                System.out.println(
+                        rs.getInt("RollNo") + "\t" +
+                        rs.getString("Name") + "\t" +
+                        rs.getString("Address"));
+            }
+
+            // First Record
+            rs.first();
+            System.out.println("\nFirst Record:");
+            System.out.println(
+                    "Row " + rs.getRow() + ": " +
+                    rs.getInt("RollNo") + "\t" +
+                    rs.getString("Name") + "\t" +
+                    rs.getString("Address"));
+
+            // Last Record
+            rs.last();
+            System.out.println("\nLast Record:");
+            System.out.println(
+                    "Row " + rs.getRow() + ": " +
+                    rs.getInt("RollNo") + "\t" +
+                    rs.getString("Name") + "\t" +
+                    rs.getString("Address"));
+
+            // Second Record from Last
+            rs.relative(-1);
+            System.out.println("\n2nd Record from Last:");
+            System.out.println(
+                    "Row " + rs.getRow() + ": " +
+                    rs.getInt("RollNo") + "\t" +
+                    rs.getString("Name") + "\t" +
+                    rs.getString("Address"));
+
+            // Second Record from Beginning
+            rs.absolute(2);
+            System.out.println("\n2nd Record from Beginning:");
+            System.out.println(
+                    "Row " + rs.getRow() + ": " +
+                    rs.getInt("RollNo") + "\t" +
+                    rs.getString("Name") + "\t" +
+                    rs.getString("Address"));
+
+            rs.close();
+            st.close();
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
